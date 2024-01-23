e231d5d2bbf6ca695d95a8ac146ec6703b71f4f6
kor <kor@dwarshuis.com>
2024-01-23 15:02:39 +0100

    First commit

---
+++ public/background.js
@@ -0,0 +1,24 @@
+chrome.action.onClicked.addListener((tab) => {
+    // Ensure that the tab ID is valid
+    if (tab.id) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['assets/index.js']
+        });
+    }
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        id: "myContextMenu",
+        title: "Info WOT-terms",
+        contexts: ["browser_action"]  // Use "browser_action" for toolbar icon
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "myContextMenu") {
+        // Execute the action you want when the menu item is clicked
+        // For example, open a new tab, show a popup, etc.
+    }
+});
