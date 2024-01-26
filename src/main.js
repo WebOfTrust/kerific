ae68c94b9bc9f4025782c53e3b306136cd4da43e
kor <kor@dwarshuis.com>
2024-01-26 18:25:05 +0100

    refactor: Remove import that is not used

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import iziToast from 'izitoast';
 
 import './assets/main.css'
 
-import * as localStorageUtil from './modules/localStorageUtil';
-
 /* 
     Use Immediately Invoked Function Expressions(IIFE): Wrap your entire code in an IIFE.This creates a new scope each time the script is run, preventing variable and function redeclarations. This is a common pattern for bookmarklets and scripts injected into pages.
 */
