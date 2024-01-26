c056e943c2de4da3b45c17834acca690c6171e3f
kor <kor@dwarshuis.com>
2024-01-26 15:46:16 +0100

    refactor: Move eventlisteners to separate file for options.html

--- src/main.js
+++ src/main.js
@@ -530,29 +530,4 @@ import * as localStorageUtil from './modules/localStorageUtil';
             console.log("Response:", response);
         });
     }
-
-    document.addEventListener('DOMContentLoaded', function () {
-
-        // Options page, log button
-        var logButton = document.getElementById('logButton');
-        logButton.addEventListener('click', function () {
-            chrome.storage.local.get(['kerificTerms'], function (result) {
-                console.log('result: ', result);
-                // if (result.kerificTerms) {
-                //     console.log('Value currently is ' + result.kerificTerms);
-                // } else {
-                //     console.log('kerificTerms not found');
-                // }
-            });
-        });
-
-        // Options page, clear button
-        var clearButton = document.getElementById('clearButton');
-        clearButton.addEventListener('click', function () {
-            chrome.runtime.sendMessage({ action: "clearStorage" }, function (response) {
-                console.log("Response:", response);
-            });
-        });
-
-    });
 })();
