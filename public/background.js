d5e028a14ff0ae63ddf1e2ea4f340188c16716ba
kor <kor@dwarshuis.com>
2024-01-26 12:56:27 +0100

    feat: Add chrome.runtime.onMessage.addListener functionality

--- public/background.js
+++ public/background.js
@@ -22,3 +22,39 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
         // For example, open a new tab, show a popup, etc.
     }
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "addTerm") {
+        // Retrieve data using Chrome Storage API
+        chrome.storage.local.get('kerificTerms', function (result) {
+            // 'result' is an object with keys as the stored keys
+            let existingData = result.kerificTerms || ""; // If no data, default to empty string
+            let newData = existingData + "\n" + request.kerificTerms;
+
+            // Store the updated data
+            chrome.storage.local.set({ kerificTerms: newData }, function () {
+                console.log('Data is stored.');
+            });
+
+            // Send a response back
+            sendResponse({ response: "Data received" });
+        });
+        return true; // For asynchronous response
+
+
+    } else if (request.action === "clearStorage") {
+        // Handle clearing storage
+        chrome.storage.local.clear(function () {
+            var error = chrome.runtime.lastError;
+            if (error) {
+                console.error(error);
+            } else {
+                sendResponse({ response: "Storage cleared" });
+            }
+        });
+        return true; // For asynchronous response
+    }
+
+    // Return true for asynchronous response, if needed
+    // return true;
+});
